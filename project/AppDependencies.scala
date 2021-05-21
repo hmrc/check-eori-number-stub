8bdab1a81cd20caecbab060a9dc66dac937af5ef
Adam Dye <31273792+adam-dye@users.noreply.github.com>
2021-05-21 11:45:54 +0100

    Updraded all libs to latest version.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,15 +6,15 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-27"  % "3.2.0",
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-27"  % "5.3.0",
     "uk.gov.hmrc"             %% "stub-data-generator"      % "0.5.3"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-27"   % "2.24.0" % Test,
-    "org.scalatest"           %% "scalatest"                % "3.1.2"                 % Test,
+    "uk.gov.hmrc"             %% "bootstrap-test-play-27"   % "4.3.0" % Test,
+    "org.scalatest"           %% "scalatest"                % "3.2.9"                 % Test,
     "com.typesafe.play"       %% "play-test"                % current                 % Test,
     "com.vladsch.flexmark"    %  "flexmark-all"             % "0.35.10"               % "test, it",
-    "org.scalatestplus.play"  %% "scalatestplus-play"       % "4.0.3"                 % "test, it"
+    "org.scalatestplus.play"  %% "scalatestplus-play"       % "4.0.0"                 % "test, it"
   )
 }
